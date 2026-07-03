e11774c6b68a9f97a185f5048ca30eaf9d78978b
Hamed Nouri <work@nouri.cc>
2026-07-02 20:41:29 -0500

    Change theme to orange, fix Download PDF hover effect

--- _source/src/app/components/layout/header/index.tsx
+++ _source/src/app/components/layout/header/index.tsx
@@ -17,7 +17,7 @@ const Header = () => {
                         <a
                             href="/files/hamed-nouri-ai-automation-communications-specialist-2026.pdf"
                             target="_blank"
-                            className="relative overflow-hidden cursor-pointer w-fit py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-primary rounded-full group"
+                            className="btn-fill relative overflow-hidden cursor-pointer w-fit py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-primary rounded-full group"
                         >
                             <span className="relative z-10 text-xl font-medium text-black group-hover:text-white transition-colors duration-300">
                                 Download PDF Resume
